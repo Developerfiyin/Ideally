a9ba97ffe39b913cc5b1d6f3702755aef333182c
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-07 21:10:39 +0100

    Created a div for signup file

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import image from "../assets/user.png";
+import image1 from "../assets/user1.png";
+import image2 from "../assets/user2.png";
+
 
 const Signup = () => {
   return (
-    <div className='bg-red-700'>Signup</div>
+    <div className='flex justify-around gap-3 py-8 px-6 '> 
+      <div className='bg-blue-700 '>
+      <img src={image} alt="The first user" />
+      </div>
+
+{/*Second div */}
+< div className='flex-col flex '> </div>
+</div>
   )
 }
 
