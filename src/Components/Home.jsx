10f542ba42899e377f27d880ea9ef24981f97de2
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-08 23:55:31 +0100

    Renamed a folder

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { createEmptyIdea } from "../hooks/Ideaflow";
+import { createEmptyIdea } from "../data/Ideaflow";
 
 const Home = () => {
   const { idea, setIdea } = createEmptyIdea();
